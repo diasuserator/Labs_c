8a465606b51e64ae72b720a6003043fab4dfac33
Dmitry Bogdan <diasuserator@yandex.ru>
2023-05-01 21:46:05 +0300

    01.05.2023:21:45

--- Project3/3.cpp
+++ Project3/3.cpp
@@ -1,31 +1,48 @@
 #define _USE_MATH_DEFINES
 #include <iostream>
 #include <cmath>
+#include <cstdio>
 using namespace std;
 
-int fTask3() {
-	cout << "Task 3" << endl;
-	int a = 0, b = 0;
-	double x = 0, y = 0, z = 0;
-	
 
-
-	//x = sqrt(a * a + b * x * cos(sqrt(x))) / (x + a * b);
-
-	if (z < a * b)
-		x = sqrt(a * a + b * b * z);
-	else
-		x = pow(sin(z), 2) + abs(a * b * z);
-
-	y = (a * x + b * x * cos(sqrt(x))) / (x + a * b);
-
-	cout << "Result" << endl << "x = " << x << "y = " << y;
-
-	return 0;
+void line(int lim) {
+	for (int i = 0; 1 < lim; i++) printf("-"); printf("\n");
 }
 
+int fTask4_1() {
+	cout << "Task 1" << endl;
+
+	int a = 0, b = 0, c = 0;
+	const float st = 0.05;
+	const double eps = 0.00001;
+	float x = -1;
+	double f = 0;
+	printf("Input a,b,c :");
+	scanf_s("%d%d%d", &a, &b, &c);
+	line(22);
+	printf("|%5c| %10c|\n", 'x', 'f');
+	line(22);
+	while (x <= (1 + eps)) {
+		if (a < 0 && c != 0) f = a * x * x + b * x + c;
+		else if (a > 0 && c == 0) {
+			if (abs(x - c) > eps) f = (-a) / (x - c);
+			else printf("| %5.2f | div by 0 |\n", x);
+		}
+		else f = a * (x + c);
+
+		printf("| %5.2f | %10.2f |\n", x, f);
+		x += st;
+	}
+	line(22);
+	return 0;
+
+}
+
+
 int main() {
-	fTask3();
+
+	fTask4_1();
 	system("pause");
 	return 0;
 }
+
